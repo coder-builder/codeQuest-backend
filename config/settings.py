464fea6c4e3db66386b41816e6717c7e835e1f02
Hobbyer <uskimeunyeob@gmail.com>
2025-10-28 19:14:53 +0900

    [김은엽] gitignore: 파일 경로 강화, settings: env의 SECRET_KEY -> DJANGO_SECRET_KEY 로 변경, users: models 작성 (User, CustomRefreshToken) User 처리 object UserManager 로 처리하도록 설정

--- config/settings.py
+++ config/settings.py
@@ -18,10 +18,10 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # .env에서 설정해놓은 시크릿키 import os를 통해 받아옴
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*'] # 모든 호스트 허용 (개발 편의를 위해, 배포 시에는 꼭 수정 필요)
 
