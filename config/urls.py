72f14df7c2a83646c112130bda97fde700fd7049
lumi <1uuuuuuuuuuuuuuumi@gmail.com>
2025-11-04 12:32:37 +0900

    [이루미] rank 앱 생성  및 기본 구조 설정

--- config/urls.py
+++ config/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 
     path('auth/', include('users.urls'), name='users'),  # users 앱의 URL
+    path('rank/', include('rank.urls'), name='rank'),
 ]
